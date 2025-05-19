c707be298672d75dd93c1063e925b7a8e49b4f70
Elly <ellygrace711@gmail.com>
2025-05-20 00:28:58 +0400

    updated tutorial page and connected skills to db

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const session = require("express-session");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-const User = require("./models/user"); // Adjust the path as necessary
-
-const { ensureSignedUp } = require("./middleware/auth");
-
 // import files to read
 const adminauth = require("./middleware/adminmdw");
 
@@ -56,56 +52,11 @@ app.use("/", adminRouter);
 const loginSignupRouter = require("./routes/login-signup");
 app.use("/", loginSignupRouter);
 
-const updateProfile = require("./routes/profileupdate");
+const updateProfile = require("./routes/userinfo");
 app.use("/", updateProfile);
 
 const userPoints = require("./routes/userpoints");
 app.use("/", userPoints);
-
-const multer = require("multer");
-
-// Set up multer for file storage
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/"); // Specify the uploads directory
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname); // Append timestamp to the filename
-  },
-});
-
-const upload = multer({ storage: storage });
-
-// Create a new route for uploading videos
-app.post(
-  "/upload-video",
-  ensureSignedUp,
-  upload.array("videos"),
-  async (req, res) => {
-    const userId = req.body.userId; // Assuming you send userId with the request
-    const videoPaths = req.files.map((file) => file.path); // Get the paths of the uploaded videos
-
-    try {
-      // Find the user and update their videos array
-      await User.findByIdAndUpdate(userId, {
-        $push: { videos: { $each: videoPaths } },
-      });
-      res
-        .status(200)
-        .json({ message: "Videos uploaded successfully", videoPaths });
-    } catch (error) {
-      res.status(500).json({ message: "Error uploading videos", error });
-    }
-  }
-);
-
-app.get("/get-user-id", (req, res) => {
-  if (req.session && req.session.userId) {
-    res.json({ userId: req.session.userId });
-  } else {
-    res.json({ userId: null });
-  }
-});
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
